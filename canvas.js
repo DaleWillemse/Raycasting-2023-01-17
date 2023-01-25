3fe520141630f8e04d1240f576d040f3dc23a7c8
Dale Willemse <DaleWillemse000@gmail.com>
2023-01-25 17:44:49 +0200

    almsot there

--- canvas.js
+++ canvas.js
@@ -7,15 +7,6 @@ function setup() {
   lines = new Lines();
 }
 
-// function changeLightColor() {
-//   if (keyCode == UP_ARROW) {
-//     let r = Math.floor(random(0, 255));
-//     let g = Math.floor(random(0, 255));
-//     let b = Math.floor(random(0, 255));
-//     light.setColor(r, g, b);
-//   }
-// }
-
 function draw() {
   background(0);
 
@@ -37,16 +28,6 @@ function mouseReleased() {
 function mouseDragged() {
   if (mouseX != pmouseX || mouseY != pmouseY) {
     lines.add(mouseX, mouseY);
-  }
-  return false;
-}
-
-function keyPressed() {
-  if (keyCode == UP_ARROW) {
-    let r = Math.floor(random(0, 255));
-    let g = Math.floor(random(0, 255));
-    let b = Math.floor(random(0, 255));
-    light.setColor(r, g, b);
   }
   return false;
 }
